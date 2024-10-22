0222facf91a6f51a4ff9590545ec04f8060e794a
elsha amalia <elshaaml14@gmail.com>
2024-10-22 16:42:06 +0700

    Update Code

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function() {
     return view('home');
 })->name('home');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard'); // 
+});
+
+
+Route::get('/verify', function () {
+    return view('auth.verify'); 
+})->name('verify.code');
+
+Route::get('/set-password', function () {
+    return view('auth.set-password'); 
+})->name('set-password');
+
+Route::get('/forgot-password', function () {
+    return view('auth.forgot-password'); 
+})->name('forgot-password');
+
