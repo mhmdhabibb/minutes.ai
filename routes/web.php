1f099b1d319ef3957904378fc5655028d091fe5e
elsha amalia <elshaaml14@gmail.com>
2024-11-26 10:36:33 +0700

    upd route

--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,6 @@ Route::get('/settings', function () {
     return view('settings'); 
 })->name('settings');
 
-Route::get('/login', function () {
-    return view('login'); 
-})->name('login');
-
-Route::get('/signup', function () {
-    return view('signup'); 
-})->name('signup');
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard')
@@ -49,3 +41,11 @@ Route::get('/set-password', function () {
 Route::get('/forgot-password', function () {
     return view('auth.forgot-password'); 
 })->name('forgot-password');
+
+Route::get('/summary', function () {
+    return view('summary'); 
+})->name('summary');
+
+Route::get('/transcript', function () {
+    return view('transcript'); 
+})->name('transcript');
